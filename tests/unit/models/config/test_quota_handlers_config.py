c081dee1b45067fccfecbca763e3952cb14538e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-01 09:46:01 +0200

    Updated and enhanced existing tests

--- tests/unit/models/config/test_quota_handlers_config.py
+++ tests/unit/models/config/test_quota_handlers_config.py
@@ -1,24 +1,57 @@
 """Unit tests for QuotaHandlersConfiguration model."""
 
+# pylint: disable=no-member
+from typing import Any
+
+import pytest
+from pydantic import ValidationError
+from pytest_subtests import SubTests
+
 from models.config import QuotaHandlersConfiguration, QuotaSchedulerConfiguration
 
 
-def test_quota_handlers_configuration() -> None:
+def test_quota_handlers_configuration(subtests: SubTests) -> None:
     """Test the quota handlers configuration."""
-    cfg = QuotaHandlersConfiguration(
-        sqlite=None,
-        postgres=None,
-        limiters=[],
-        scheduler=QuotaSchedulerConfiguration(
-            database_reconnection_count=10,
-            database_reconnection_delay=60,
-            period=10,
-        ),
-        enable_token_history=False,
-    )
-    assert cfg is not None
-    assert cfg.sqlite is None
-    assert cfg.postgres is None
-    assert cfg.limiters == []
-    assert cfg.scheduler is not None
-    assert not cfg.enable_token_history
+    with subtests.test(msg="Token history disabled"):
+        cfg = QuotaHandlersConfiguration(
+            sqlite=None,
+            postgres=None,
+            limiters=[],
+            scheduler=QuotaSchedulerConfiguration(
+                database_reconnection_count=10,
+                database_reconnection_delay=60,
+                period=10,
+            ),
+            enable_token_history=False,
+        )
+        assert cfg is not None
+        assert cfg.sqlite is None
+        assert cfg.postgres is None
+        assert cfg.limiters == []
+        assert cfg.scheduler is not None
+        assert cfg.scheduler.database_reconnection_count == 10
+        assert cfg.scheduler.database_reconnection_delay == 60
+        assert cfg.scheduler.period == 10
+        assert not cfg.enable_token_history
+
+    with subtests.test(msg="Token history enabled"):
+        cfg = QuotaHandlersConfiguration(
+            sqlite=None,
+            postgres=None,
+            limiters=[],
+            scheduler=QuotaSchedulerConfiguration(
+                database_reconnection_count=10,
+                database_reconnection_delay=60,
+                period=10,
+            ),
+            enable_token_history=True,
+        )
+        assert cfg is not None
+        assert cfg.sqlite is None
+        assert cfg.postgres is None
+        assert cfg.limiters == []
+        assert cfg.scheduler is not None
+        assert cfg.scheduler.database_reconnection_count == 10
+        assert cfg.scheduler.database_reconnection_delay == 60
+        assert cfg.scheduler.period == 10
+        assert cfg.enable_token_history
