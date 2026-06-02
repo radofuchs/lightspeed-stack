ef17f40bdd055ef9e563b5022c27b3d32fa3c010
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-02 09:10:51 +0200

    Refactored existing positive and negative tests

--- tests/unit/models/config/test_quota_limiter_config.py
+++ tests/unit/models/config/test_quota_limiter_config.py
@@ -1,79 +1,163 @@
 """Unit tests for QuotaLimiterConfig model."""
 
 import pytest
+from pytest_subtests import SubTests
 
 from models.config import QuotaLimiterConfiguration
 
 
-def test_quota_limiter_configuration() -> None:
+def test_quota_limiter_configuration(subtests: SubTests) -> None:
     """Test the default configuration."""
-    cfg = QuotaLimiterConfiguration(
-        type="cluster_limiter",
-        name="cluster_monthly_limits",
-        initial_quota=0,
-        quota_increase=10,
-        period="3 seconds",
-    )
-    assert cfg is not None
-    assert cfg.type == "cluster_limiter"
-    assert cfg.name == "cluster_monthly_limits"
-    assert cfg.initial_quota == 0
-    assert cfg.quota_increase == 10
-    assert cfg.period == "3 seconds"
-
-
-def test_quota_limiter_configuration_improper_value_1() -> None:
-    """Test the default configuration.
-
-    Verify that constructing a QuotaLimiterConfiguration with a negative
-    `initial_quota` raises a ValueError with message "Input should be greater
-    than or equal to 0".
-    """
-    with pytest.raises(ValueError, match="Input should be greater than or equal to 0"):
-        _ = QuotaLimiterConfiguration(
+    with subtests.test(msg="Zero initial quota"):
+        cfg = QuotaLimiterConfiguration(
             type="cluster_limiter",
             name="cluster_monthly_limits",
-            initial_quota=-1,
+            initial_quota=0,
             quota_increase=10,
             period="3 seconds",
         )
+        assert cfg is not None
+        assert cfg.type == "cluster_limiter"
+        assert cfg.name == "cluster_monthly_limits"
+        assert cfg.initial_quota == 0
+        assert cfg.quota_increase == 10
+        assert cfg.period == "3 seconds"
 
-
-def test_quota_limiter_configuration_improper_value_2() -> None:
-    """Test the default configuration.
-
-    Verify that providing a negative `quota_increase` raises a ValueError.
-
-    Asserts that constructing a QuotaLimiterConfiguration with `quota_increase`
-    less than zero raises a ValueError with the message "Input should be
-    greater than or equal to 0".
-    """
-    with pytest.raises(ValueError, match="Input should be greater than or equal to 0"):
-        _ = QuotaLimiterConfiguration(
+    with subtests.test(msg="Non zero initial quota"):
+        cfg = QuotaLimiterConfiguration(
             type="cluster_limiter",
             name="cluster_monthly_limits",
-            initial_quota=1,
-            quota_increase=-10,
-            period="3 seconds",
-        )
-
-
-def test_quota_limiter_configuration_improper_value_3() -> None:
-    """Test the default configuration.
-
-    Check that constructing QuotaLimiterConfiguration with an invalid `type`
-    raises a ValueError with the expected message.
-
-    Raises:
-        ValueError: if `type` is not 'user_limiter' or 'cluster_limiter'.
-    """
-    with pytest.raises(
-        ValueError, match="Input should be 'user_limiter' or 'cluster_limiter'"
-    ):
-        _ = QuotaLimiterConfiguration(
-            type="unknown_limiter",  # pyright: ignore[reportArgumentType]
-            name="cluster_monthly_limits",
-            initial_quota=1,
+            initial_quota=42,
             quota_increase=10,
             period="3 seconds",
         )
+        assert cfg is not None
+        assert cfg.type == "cluster_limiter"
+        assert cfg.name == "cluster_monthly_limits"
+        assert cfg.initial_quota == 42
+        assert cfg.quota_increase == 10
+        assert cfg.period == "3 seconds"
+
+    with subtests.test(msg="Zero quota increase"):
+        cfg = QuotaLimiterConfiguration(
+            type="cluster_limiter",
+            name="cluster_monthly_limits",
+            initial_quota=10,
+            quota_increase=0,
+            period="3 seconds",
+        )
+        assert cfg is not None
+        assert cfg.type == "cluster_limiter"
+        assert cfg.name == "cluster_monthly_limits"
+        assert cfg.initial_quota == 10
+        assert cfg.quota_increase == 0
+        assert cfg.period == "3 seconds"
+
+    with subtests.test(msg="Non zero quota increase"):
+        cfg = QuotaLimiterConfiguration(
+            type="cluster_limiter",
+            name="cluster_monthly_limits",
+            initial_quota=10,
+            quota_increase=42,
+            period="3 seconds",
+        )
+        assert cfg is not None
+        assert cfg.type == "cluster_limiter"
+        assert cfg.name == "cluster_monthly_limits"
+        assert cfg.initial_quota == 10
+        assert cfg.quota_increase == 42
+        assert cfg.period == "3 seconds"
+
+    with subtests.test(msg="User limiter"):
+        cfg = QuotaLimiterConfiguration(
+            type="user_limiter",
+            name="user_monthly_limits",
+            initial_quota=10,
+            quota_increase=42,
+            period="3 seconds",
+        )
+        assert cfg is not None
+        assert cfg.type == "user_limiter"
+        assert cfg.name == "user_monthly_limits"
+        assert cfg.initial_quota == 10
+        assert cfg.quota_increase == 42
+        assert cfg.period == "3 seconds"
+
+
+def test_quota_limiter_configuration_improper_values(subtests: SubTests) -> None:
+    """Test the quota limiter configuration initialization with improper values."""
+    with subtests.test(msg="Negative initial quota value"):
+        # Verify that providing a negative `initial_quota` raises a ValueError.
+        #
+        # Verify that constructing a QuotaLimiterConfiguration with a negative
+        # `initial_quota` raises a ValueError with message "Input should be
+        # greater than or equal to 0".
+        with pytest.raises(
+            ValueError, match="Input should be greater than or equal to 0"
+        ):
+            _ = QuotaLimiterConfiguration(
+                type="cluster_limiter",
+                name="cluster_monthly_limits",
+                initial_quota=-1,
+                quota_increase=10,
+                period="3 seconds",
+            )
+
+    with subtests.test(msg="Negative quota increase value"):
+        # Verify that providing a negative `quota_increase` raises a ValueError.
+
+        # Asserts that constructing a QuotaLimiterConfiguration with `quota_increase`
+        # less than zero raises a ValueError with the message "Input should be
+        # greater than or equal to 0".
+        with pytest.raises(
+            ValueError, match="Input should be greater than or equal to 0"
+        ):
+            _ = QuotaLimiterConfiguration(
+                type="cluster_limiter",
+                name="cluster_monthly_limits",
+                initial_quota=1,
+                quota_increase=-10,
+                period="3 seconds",
+            )
+
+    with subtests.test(msg="Negative initial quota and quota increase value"):
+        # Verify that providing a negative `quota_increase` raises a ValueError.
+
+        # Asserts that constructing a QuotaLimiterConfiguration with `quota_increase`
+        # less than zero raises a ValueError with the message "Input should be
+        # greater than or equal to 0".
+        with pytest.raises(
+            ValueError, match="Input should be greater than or equal to 0"
+        ):
+            _ = QuotaLimiterConfiguration(
+                type="cluster_limiter",
+                name="cluster_monthly_limits",
+                initial_quota=-1,
+                quota_increase=-10,
+                period="3 seconds",
+            )
+
+    with subtests.test(msg="Improper quota limiter"):
+        # Check that constructing QuotaLimiterConfiguration with an invalid `type`
+        # raises a ValueError with the expected message.
+        with pytest.raises(
+            ValueError, match="Input should be 'user_limiter' or 'cluster_limiter'"
+        ):
+            _ = QuotaLimiterConfiguration(
+                type="unknown_limiter",  # pyright: ignore[reportArgumentType]
+                name="cluster_monthly_limits",
+                initial_quota=1,
+                quota_increase=10,
+                period="3 seconds",
+            )
+
+    with subtests.test(msg="Improper period"):
+        with pytest.raises(ValueError, match="Input should be a valid string"):
+            _ = QuotaLimiterConfiguration(
+                type="cluster_limiter",
+                name="cluster_monthly_limits",
+                initial_quota=1,
+                quota_increase=10,
+                period=3,
+            )
+
