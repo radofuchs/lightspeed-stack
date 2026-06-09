739491ac3f7c6f9b08031ec08df5e652fc2ddd4a
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-06-09 08:31:03 +0200

    Updated descriptions/docstrings

--- tests/benchmarks/test_token_estimator.py
+++ tests/benchmarks/test_token_estimator.py
@@ -17,49 +17,122 @@ culpa qui officia deserunt mollit anim id est laborum.
 
 
 def test_estimate_empty_string(benchmark: BenchmarkFixture) -> None:
-    """Benchmark for empty string as input."""
+    """Benchmark for tokenizing empty string.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     input_string = ""
     benchmark(estimate_tokens, input_string)
 
 
 def test_estimate_hello_world(benchmark: BenchmarkFixture) -> None:
-    """Benchmark for Hello world as input."""
+    """Benchmark for tokenizing the string Hello world.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     input_string = "Hello world"
     benchmark(estimate_tokens, input_string)
 
 
 def test_pangram(benchmark: BenchmarkFixture) -> None:
-    """The pangram tokenizes to the known cl100k_base count."""
+    """Benchmark for tokenizing known English pangram.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     input_string = "The quick brown fox jumps over the lazy dog."
     benchmark(estimate_tokens, input_string)
 
 
 def test_lorem_ipsum(benchmark: BenchmarkFixture) -> None:
-    """The lorem ipsum tokenizes to the known cl100k_base count."""
+    """Benchmark for tokenizing Lorem Ipsum text.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     input_string = LOREM_IPSUM
     benchmark(estimate_tokens, input_string)
 
 
 def test_lorem_ipsum_times_10_times(benchmark: BenchmarkFixture) -> None:
-    """The lorem ipsum tokenizes to the known cl100k_base count."""
+    """Benchmark for tokenizing Lorem Ipsum text repeated ten times.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     input_string = LOREM_IPSUM * 10
     benchmark(estimate_tokens, input_string)
 
 
 def test_lorem_ipsum_times_100_times(benchmark: BenchmarkFixture) -> None:
-    """The lorem ipsum tokenizes to the known cl100k_base count."""
+    """Benchmark for tokenizing Lorem Ipsum text repeated 100x.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     input_string = LOREM_IPSUM * 100
     benchmark(estimate_tokens, input_string)
 
 
-def _test_lorem_ipsum_times_1000_times(benchmark: BenchmarkFixture) -> None:
-    """The lorem ipsum tokenizes to the known cl100k_base count."""
+def test_lorem_ipsum_times_1000_times(benchmark: BenchmarkFixture) -> None:
+    """Benchmark for tokenizing Lorem Ipsum text repeated 1000x.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     input_string = LOREM_IPSUM * 1000
     benchmark(estimate_tokens, input_string)
 
 
 def benchmark_file_tokenization(benchmark: BenchmarkFixture, filename: str) -> None:
-    """Read the given file and tokenize it."""
+    """Read the given file and tokenize it as benchmark.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+        filename: name of file whose content should be tokenized.
+
+    Returns:
+    -------
+        None
+    """
     with open("tests/benchmarks/data/" + filename, encoding="utf-8") as fin:
         input_string = fin.read()
         # tokenize the file content
@@ -67,20 +140,70 @@ def benchmark_file_tokenization(benchmark: BenchmarkFixture, filename: str) -> N
 
 
 def test_xml_file_10_lines(benchmark: BenchmarkFixture) -> None:
-    """Test tokenizing XML file containing just 10 lines."""
+    """Test tokenizing XML file containing just 10 lines.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     benchmark_file_tokenization(benchmark, "xml_10_lines.xml")
 
 
 def test_yaml_file_10_lines(benchmark: BenchmarkFixture) -> None:
-    """Test tokenizing YAML file containing just 10 lines."""
+    """Test tokenizing YAML file containing just 10 lines.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     benchmark_file_tokenization(benchmark, "yaml_10_lines.yml")
 
 
 def test_json_file_10_lines(benchmark: BenchmarkFixture) -> None:
-    """Test tokenizing JSON file containing just 10 lines."""
+    """Test tokenizing JSON file containing just 10 lines.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     benchmark_file_tokenization(benchmark, "json_10_lines.json")
 
 
 def test_python_source_10_lines(benchmark: BenchmarkFixture) -> None:
-    """Test tokenizing Python script containing just 10 lines."""
+    """Test tokenizing Python script containing just 10 lines.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
     benchmark_file_tokenization(benchmark, "python_10_lines.py")
+
+
+def test_javascript_source_10_lines(benchmark: BenchmarkFixture) -> None:
+    """Test tokenizing JavaScript script containing just 10 lines.
+
+    Parameters:
+    ----------
+        benchmark (BenchmarkFixture): pytest-benchmark fixture.
+
+    Returns:
+    -------
+        None
+    """
+    benchmark_file_tokenization(benchmark, "js_10_lines.js")
