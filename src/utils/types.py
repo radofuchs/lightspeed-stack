3ea3ca472752370dc4d74b3ee92721707e7b4521
Pavel Tisnovsky <ptisnovs@redhat.com>
2026-09-01 09:07:48 +0200

    Removed unused function content_to_str

--- src/utils/types.py
+++ src/utils/types.py
@@ -10,34 +10,6 @@ type SingletonInstances = dict[type, Any]
 CompiledPatterns = list[tuple[Pattern[str], str]]
 
 
-def content_to_str(content: Any) -> str:
-    """Convert content (str, TextContentItem, ImageContentItem, or list) to string.
-
-    Parameters:
-    ----------
-        content: Value to normalize into a string (may be None,
-                 str, content item, list, or any other object).
-
-    Returns:
-    -------
-        str: The normalized string representation of the content.
-    """
-    match content:
-        case None:
-            return ""
-        case str():
-            return content
-        case TextContentItem():
-            # help the type checkers to infer return data type
-            return str(content.text)
-        case ImageContentItem():
-            return "<image>"
-        case list():
-            return " ".join(content_to_str(item) for item in content)
-        case _:
-            return str(content)
-
-
 class Singleton(type):
     """Metaclass for Singleton support."""
 
